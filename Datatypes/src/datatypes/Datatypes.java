c1bfa84dda540aeca8e23ef138feb18304569125
bibo <bibo0088@abv.bg>
2019-05-30 12:23:44 +0300

    PrimitiveDatatypes

--- Datatypes/src/datatypes/Datatypes.java
+++ Datatypes/src/datatypes/Datatypes.java
@@ -29,7 +29,7 @@ public class Datatypes {
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
                 if(x>=-32768 && x<=32767)System.out.println("* short");
-                 if(x>=-2147483648l && x<=2147483647l)System.out.println("* int");
+                 if(x>=-2147483648L && x<=2147483647L)System.out.println("* int");
                   if(x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
             }
             catch(Exception e)
