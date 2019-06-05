64b1b6341fdcc8e5fba9a72c858bad9300860ef1
bibo <bibo0088@abv.bg>
2019-06-05 12:58:13 +0300

    Substring

---
+++ JavaSubstring/src/javasubstring/JavaSubstring.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasubstring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bibo
+ */
+public class JavaSubstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        System.out.println(S.substring(start, end));
+    }
+    
+}
