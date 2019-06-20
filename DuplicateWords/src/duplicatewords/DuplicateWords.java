cbf79c3a672227b32d667ed2b5a6d0f4f5621363
bibo <bibo0088@abv.bg>
2019-06-20 12:01:45 +0300

    Duplicate Words

--- DuplicateWords/src/duplicatewords/DuplicateWords.java
+++ DuplicateWords/src/duplicatewords/DuplicateWords.java
@@ -19,23 +19,23 @@ public class DuplicateWords {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      String pattern = "(\\b\\w+\\b)(\\s*\\1\\b)+";
-             Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+        String pattern = "(\\b\\w+\\b)(\\s*\\1\\b)+";
+        Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
 
-            Scanner in = new Scanner(System.in);
-            int testCases = Integer.parseInt(in.nextLine());
-            while(testCases>0){
-                String input = in.nextLine();
-                Matcher m = r.matcher(input);
-                boolean findMatch = true;
-                while(m.find( )){
-                   input = input.replaceAll(m.group(), m.group(1));
-                    findMatch = false;
-                }
-                
-                System.out.println(input);
-                testCases--;
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String input = in.nextLine();
+            Matcher m = r.matcher(input);
+            boolean findMatch = true;
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+                findMatch = false;
             }
+
+            System.out.println(input);
+            testCases--;
+        }
     }
-    
+
 }
