866296a3db64284a11774890fcb1ed9101c82883
bibo <bibo0088@abv.bg>
2019-09-05 13:30:20 +0300

    Polymorphism

---
+++ Polymorph/src/polymorph/Polymorph.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorph;
+
+class Bike{  
+  void run(){System.out.println("running");}  
+}  
+class Splendor extends Bike{  
+  void run(){System.out.println("running safely with 60km");}  
+  
+  public static void main(String args[]){  
+    Splendor b = new Splendor();//upcasting  
+    b.run();  
+  }  
+}  
